9be0db6d2fd47400fbc9798187dba8496a6fe2a4
Brandon Blaylock <brandon@null.pub>
2019-06-05 15:39:03 -0700

    docs: added link documentation from openapi io-ts codecs to spec

--- src/types/openapi-3.0.2.ts
+++ src/types/openapi-3.0.2.ts
@@ -7,33 +7,58 @@ import { createOptionFromNullable, fromNullable, mapOutput } from 'io-ts-types';
  */
 const SEMVER_REGEX = /(?<=^v?|\sv?)(?:0|[1-9]\d*)\.(?:0|[1-9]\d*)\.(?:0|[1-9]\d*)(?:-(?:[1-9]\d*|[\da-z-]*[a-z-][\da-z-]*)(?:\.(?:[1-9]\d*|[\da-z-]*[a-z-][\da-z-]*))*)?(?:\+[\da-z-]+(?:\.[\da-z-]+)*)?(?=$|\s)/gi;
 
+/**
+ * Semver Refinement type to ensure that semver field matches the specification
+ */
 interface SemverBrand {
-  readonly Semver: unique symbol; // use `unique symbol` here to ensure uniqueness across modules / packages
+  readonly Semver: unique symbol;
 }
-
 const Semver = t.brand(
-  t.string, // a codec representing the type to be refined
-  (n): n is t.Branded<string, SemverBrand> => SEMVER_REGEX.test(n), // a custom type guard using the build-in helper `Branded`
-  'Semver' // the name must match the readonly field in the brand
+  t.string,
+  (n): n is t.Branded<string, SemverBrand> => SEMVER_REGEX.test(n),
+  'Semver'
 );
 type Semver = t.TypeOf<typeof Semver>;
 
+/**
+ * Helper Type to pull the Encode and Decode generics from a Codec
+ */
 type CodecOf<T> = T extends t.Type<infer A, infer O, infer I>
   ? [A, O, I]
   : never;
 
+/**
+ * Helper function to turn empty arrays into undefined on output
+ */
 const toUndefined = <A>(x: A | null): A | undefined =>
   Array.isArray(x) && x.length === 0 ? undefined : x;
 
+/**
+ * Higer order codec to default nullable booleans to false
+ */
 const nullableBooleanFalse = fromNullable(t.boolean)(false);
+
+/**
+ * Higher order codec to default nullable arrays of codec T to empty arrays
+ * on decode.
+ */
 const nullableArray = <A, O = A, I = unknown>(codec: t.Type<A, O, I>) =>
   mapOutput(fromNullable(t.array(codec))([]), toUndefined);
 
 /**
- * io-ts types for openapi 3.0.2
+ * io-ts codecs for openapi 3.0.2
+ *
+ * These types are used to validate openapi 3.0.x json and cast it into
+ * strong typescript types if it validates.
+ *
  * https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md
  */
 
+/**
+ * Reference Object
+ *
+ * https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#referenceObject
+ */
 export const ReferenceObjectIO = t.type({
   $ref: t.string,
 });
@@ -46,12 +71,22 @@ export const ContactObjectIO = t.type({
 });
 export type ContactObject = t.TypeOf<typeof ContactObjectIO>;
 
+/**
+ * License Object
+ *
+ * https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#licenseObject
+ */
 export const LicenseObjectIO = t.type({
   name: t.string,
   url: createOptionFromNullable(t.string),
 });
 export type LicenseObject = t.TypeOf<typeof LicenseObjectIO>;
 
+/**
+ * Info Object
+ *
+ * https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#InfoObject
+ */
 export const InfoObjectIO = t.type({
   title: t.string,
   description: createOptionFromNullable(t.string),
@@ -62,6 +97,11 @@ export const InfoObjectIO = t.type({
 });
 export type InfoObject = t.TypeOf<typeof InfoObjectIO>;
 
+/**
+ * Server Variable Object
+ *
+ * https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#ServerVariableObject
+ */
 export const ServerVariableObjectIO = t.type({
   enum: createOptionFromNullable(
     t.union([t.array(t.string), t.array(t.boolean), t.array(t.number)])
@@ -71,6 +111,11 @@ export const ServerVariableObjectIO = t.type({
 });
 export type ServerVariableObject = t.TypeOf<typeof ServerVariableObjectIO>;
 
+/**
+ * Server Object
+ *
+ * https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#ServerObject
+ */
 export const ServerObjectIO = t.type({
   url: t.string,
   description: createOptionFromNullable(t.string),
@@ -80,6 +125,11 @@ export const ServerObjectIO = t.type({
 });
 export type ServerObject = t.TypeOf<typeof ServerObjectIO>;
 
+/**
+ * External Documentation Object
+ *
+ * https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#ExternalDocumentationObject
+ */
 export const ExternalDocumentationObjectIO = t.type({
   url: t.string,
   description: createOptionFromNullable(t.string),
@@ -91,6 +141,11 @@ export type ExternalDocumentationObjectO = CodecOf<
   typeof ExternalDocumentationObjectIO
 >[1];
 
+/**
+ * Parameter Location
+ *
+ * https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#parameter-locations
+ */
 export const ParameterLocationIO = t.union([
   t.literal('query'),
   t.literal('header'),
@@ -99,6 +154,11 @@ export const ParameterLocationIO = t.union([
 ]);
 export type ParameterLocation = t.TypeOf<typeof ParameterLocationIO>;
 
+/**
+ * Parameter Style
+ *
+ * https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#ParameterStyle
+ */
 export const ParameterStyleIO = t.union([
   t.literal('matrix'),
   t.literal('label'),
@@ -110,6 +170,11 @@ export const ParameterStyleIO = t.union([
 ]);
 export type ParameterStyle = t.TypeOf<typeof ParameterStyleIO>;
 
+/**
+ * Exmaple Object
+ *
+ * https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#exampleObject
+ */
 export const ExampleObjectIO = t.type({
   summary: createOptionFromNullable(t.string),
   description: createOptionFromNullable(t.string),
@@ -118,9 +183,19 @@ export const ExampleObjectIO = t.type({
 });
 export type ExmapleObject = t.TypeOf<typeof ExampleObjectIO>;
 
+/**
+ * Link Parameters Object
+ *
+ * https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#linkObject
+ */
 export const LinkParametersObjectIO = t.record(t.string, t.any);
 export type LinkParametersObject = t.TypeOf<typeof LinkParametersObjectIO>;
 
+/**
+ * Link Object
+ *
+ * https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#linkObject
+ */
 export const LinkObjectIO = t.type({
   operationRef: createOptionFromNullable(t.string),
   operationId: createOptionFromNullable(t.string),
@@ -131,12 +206,22 @@ export const LinkObjectIO = t.type({
 });
 export type LinkObject = t.TypeOf<typeof LinkObjectIO>;
 
+/**
+ * Links Object
+ *
+ * https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#linkObject
+ */
 export const LinksObjectIO = t.record(
   t.string,
   t.union([LinkObjectIO, ReferenceObjectIO])
 );
 export type LinksObject = t.TypeOf<typeof LinksObjectIO>;
 
+/**
+ * Tag Object
+ *
+ * https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#TagObject
+ */
 export const TagObjectIO = t.type({
   name: t.string,
   description: createOptionFromNullable(t.string),
@@ -144,12 +229,22 @@ export const TagObjectIO = t.type({
 });
 export type TagObject = t.TypeOf<typeof TagObjectIO>;
 
+/**
+ * Examples Object
+ *
+ * https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#exampleObject
+ */
 export const ExamplesObjectIO = t.record(
   t.string,
   t.union([ExampleObjectIO, ReferenceObjectIO])
 );
 export type ExamplesObject = t.TypeOf<typeof ExamplesObjectIO>;
 
+/**
+ * Schema Object
+ *
+ * https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#SchemaObject
+ */
 export type SchemaObject = {
   nullable: boolean;
   discriminator: Option<DiscriminatorObject>;
@@ -280,6 +375,11 @@ export const SchemaObjectIO: t.Type<SchemaObject, SchemaObjectO> = t.recursion(
     })
 );
 
+/**
+ * Media Type Object
+ *
+ * https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#MediaTypeObject
+ */
 export const MediaTypeObjectIO = t.type({
   schema: createOptionFromNullable(
     t.union([SchemaObjectIO, ReferenceObjectIO])
@@ -290,9 +390,19 @@ export const MediaTypeObjectIO = t.type({
 });
 export type MediaTypeObject = t.TypeOf<typeof MediaTypeObjectIO>;
 
+/**
+ * Content Object
+ *
+ * https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#media-type-object
+ */
 export const ContentObjectIO = t.record(t.string, MediaTypeObjectIO);
 export type ContentObject = t.TypeOf<typeof ContentObjectIO>;
 
+/**
+ * Base Parameter Object
+ *
+ * https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#parameterObject
+ */
 export const BaseParameterObjectIO = t.type({
   description: createOptionFromNullable(t.string),
   required: nullableBooleanFalse,
@@ -312,15 +422,30 @@ export const BaseParameterObjectIO = t.type({
 });
 export type BaseParameterObject = t.TypeOf<typeof BaseParameterObjectIO>;
 
+/**
+ * Header Object
+ *
+ * https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#HeaderObject
+ */
 export const HeaderObjectIO = BaseParameterObjectIO;
 export type HeaderObject = t.TypeOf<typeof HeaderObjectIO>;
 
+/**
+ * Headers Object
+ *
+ * https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#HeaderObject
+ */
 export const HeadersObjectIO = t.record(
   t.string,
   t.union([HeaderObjectIO, ReferenceObjectIO])
 );
 export type HeadersObject = t.TypeOf<typeof HeadersObjectIO>;
 
+/**
+ * Response Object
+ *
+ * https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#ResponseObject
+ */
 export const ResponseObjectIO = t.type({
   description: t.string,
   headers: createOptionFromNullable(HeadersObjectIO),
@@ -329,6 +454,11 @@ export const ResponseObjectIO = t.type({
 });
 export type ResponseObject = t.TypeOf<typeof ResponseObjectIO>;
 
+/**
+ * Responses Object
+ *
+ * https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#ResponseObject
+ */
 export const ResponsesObjectIO = t.union([
   t.type({
     default: createOptionFromNullable(
@@ -339,6 +469,11 @@ export const ResponsesObjectIO = t.union([
 ]);
 export type ResponsesObject = t.TypeOf<typeof ResponsesObjectIO>;
 
+/**
+ * Encoding Property Object
+ *
+ * https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#encodingObject
+ */
 export const EncodingPropertyObjectIO = t.type({
   contentType: createOptionFromNullable(t.string),
   headers: createOptionFromNullable(
@@ -350,9 +485,19 @@ export const EncodingPropertyObjectIO = t.type({
 });
 export type EncodingPropertyObject = t.TypeOf<typeof EncodingPropertyObjectIO>;
 
+/**
+ * Encoding Object
+ *
+ * https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#encodingObject
+ */
 export const EncodingObjectIO = t.record(t.string, EncodingPropertyObjectIO);
 export type EncodingObject = t.TypeOf<typeof EncodingObjectIO>;
 
+/**
+ * Request Body Object
+ *
+ * https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#RequestBodyObject
+ */
 export const RequestBodyObjectIO = t.type({
   description: createOptionFromNullable(t.string),
   content: ContentObjectIO,
@@ -360,9 +505,19 @@ export const RequestBodyObjectIO = t.type({
 });
 export type RequestBodyObject = t.TypeOf<typeof RequestBodyObjectIO>;
 
+/**
+ * Schemas Object
+ *
+ * https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#SchemaObject
+ */
 export const SchemasObjectIO = t.record(t.string, SchemaObjectIO);
 export type SchemasObject = t.TypeOf<typeof SchemasObjectIO>;
 
+/**
+ * Paramater Object
+ *
+ * https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#parameterObject
+ */
 export const ParameterObjectIO = t.union([
   BaseParameterObjectIO,
   t.type({
@@ -370,8 +525,13 @@ export const ParameterObjectIO = t.union([
     in: ParameterLocationIO,
   }),
 ]);
-export type ParamaterObject = t.TypeOf<typeof ParameterObjectIO>;
+export type ParameterObject = t.TypeOf<typeof ParameterObjectIO>;
 
+/**
+ * Discriminator Object
+ *
+ * https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#DiscriminatorObject
+ */
 export const DiscriminatorObjectIO = t.type({
   propertyName: t.string,
   mapping: createOptionFromNullable(t.record(t.string, t.string)),
@@ -379,6 +539,11 @@ export const DiscriminatorObjectIO = t.type({
 export type DiscriminatorObject = t.TypeOf<typeof DiscriminatorObjectIO>;
 export type DiscriminatorObjectO = CodecOf<typeof DiscriminatorObjectIO>[1];
 
+/**
+ * Xml Object
+ *
+ * https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#XmlObject
+ */
 export const XmlObjectIO = t.type({
   name: createOptionFromNullable(t.string),
   namespace: createOptionFromNullable(t.string),
@@ -389,6 +554,11 @@ export const XmlObjectIO = t.type({
 export type XmlObject = t.TypeOf<typeof XmlObjectIO>;
 export type XmlObjectO = CodecOf<typeof XmlObjectIO>[1];
 
+/**
+ * Security Scheme Type
+ *
+ * https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#SecuritySchemeType
+ */
 export const SecuritySchemeTypeIO = t.union([
   t.literal('apiKey'),
   t.literal('http'),
@@ -397,9 +567,19 @@ export const SecuritySchemeTypeIO = t.union([
 ]);
 export type SecuritySchemeType = t.TypeOf<typeof SecuritySchemeTypeIO>;
 
+/**
+ * Scopes Object
+ *
+ * https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#oauth-flows-object
+ */
 export const ScopesObjectIO = t.record(t.string, t.string);
 export type ScopesObject = t.TypeOf<typeof ScopesObjectIO>;
 
+/**
+ * OAuth Flow Object
+ *
+ * https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#oauth-flows-object
+ */
 export const OAuthFlowObjectIO = t.type({
   authorizationUrl: createOptionFromNullable(t.string),
   tokenUrl: createOptionFromNullable(t.string),
@@ -408,6 +588,11 @@ export const OAuthFlowObjectIO = t.type({
 });
 export type OAuthFlowObject = t.TypeOf<typeof OAuthFlowObjectIO>;
 
+/**
+ * OAuth Flows Object
+ *
+ * https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#oauth-flows-object
+ */
 export const OAuthFlowsObjectIO = t.type({
   implicit: createOptionFromNullable(OAuthFlowObjectIO),
   password: createOptionFromNullable(OAuthFlowObjectIO),
@@ -416,6 +601,11 @@ export const OAuthFlowsObjectIO = t.type({
 });
 export type OAuthFlowsObject = t.TypeOf<typeof OAuthFlowsObjectIO>;
 
+/**
+ * Security Scheme Object
+ *
+ * https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#SecuritySchemeObject
+ */
 export const SecuritySchemeObjectIO = t.type({
   type: SecuritySchemeTypeIO,
   description: createOptionFromNullable(t.string),
@@ -428,11 +618,21 @@ export const SecuritySchemeObjectIO = t.type({
 });
 export type SecuritySchemeObject = t.TypeOf<typeof SecuritySchemeObjectIO>;
 
+/**
+ * Security Requirement Object
+ *
+ * https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#SecurityRequirementObject
+ */
 export const SecurityRequirementObjectIO = t.record(t.string, t.string);
 export type SecurityRequirementObject = t.TypeOf<
   typeof SecurityRequirementObjectIO
 >;
 
+/**
+ * Operation Object
+ *
+ * https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#OperationObject
+ */
 export const OperationObjectIO = t.type({
   tags: nullableArray(t.string),
   summary: createOptionFromNullable(t.string),
@@ -453,6 +653,11 @@ export const OperationObjectIO = t.type({
 });
 export type OperationObject = t.TypeOf<typeof OperationObjectIO>;
 
+/**
+ * Path Item Object
+ *
+ * https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#PathItemObject
+ */
 export const PathItemObjectIO = t.type({
   $ref: createOptionFromNullable(t.string),
   summary: createOptionFromNullable(t.string),
@@ -472,18 +677,38 @@ export const PathItemObjectIO = t.type({
 });
 export type PathItemObject = t.TypeOf<typeof PathItemObjectIO>;
 
+/**
+ * Paths Object
+ *
+ * https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#PathItemObject
+ */
 export const PathsObjectIO = t.record(t.string, PathItemObjectIO);
 export type PathsObject = t.TypeOf<typeof PathsObjectIO>;
 
+/**
+ * Callback Object
+ *
+ * https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#CallbackObject
+ */
 export const CallbackObjectIO = t.record(t.string, PathItemObjectIO);
 export type CallbackObject = t.TypeOf<typeof CallbackObjectIO>;
 
+/**
+ * Callbacks Object
+ *
+ * https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#CallbackObject
+ */
 export const CallbacksObjectIO = t.record(
   t.string,
   t.union([CallbackObjectIO, ReferenceObjectIO])
 );
 export type CallbacksObject = t.TypeOf<typeof CallbacksObjectIO>;
 
+/**
+ * Components Object
+ *
+ * https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#ComponentsObject
+ */
 export const ComponentsObjectIO = t.type({
   schemas: createOptionFromNullable(
     t.record(t.string, t.union([SchemaObjectIO, ReferenceObjectIO]))
@@ -515,6 +740,11 @@ export const ComponentsObjectIO = t.type({
 });
 export type ComponentsObject = t.TypeOf<typeof ComponentsObjectIO>;
 
+/**
+ * Open API Object
+ *
+ * https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#openapi-object
+ */
 export const OpenAPIObjectIO = t.type({
   openapi: Semver,
   info: InfoObjectIO,
